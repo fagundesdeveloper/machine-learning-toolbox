688dbe9d904c7657fef24acce93cbda702e5f3a4
nohman20 <nohman20@gmail.com>
2016-07-12 17:33:28 -0700

    Webpack config and entry point working

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,26 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/food_hack/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
 
+module.exports = {
+  entry: [
+    './food_hack/index.js'
+  ],
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins: [HTMLWebpackPluginConfig]
+}
