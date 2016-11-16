aaf6b61633092e73866f38931074dc9add3680d1
nohmapp <nohman20@gmail.com>
2016-11-16 14:07:40 -0800

    creates json for player queries

--- queryNBA.js
+++ queryNBA.js
@@ -1,13 +1,37 @@
 const nba = require('nba.js').default
 const fs = require('fs')
 
-// returns json
-nba.stats.allPlayers().then(res => {
-  var data = JSON.stringify(res, null, '\t')
+nba.stats.playerCareerStats({ LeagueID: '00', PerMode: 'PerGame', PlayerID: '203954' })
+  .then(res => {
+    var data = JSON.stringify(res, null, '\t')
 
-  fs.writeFile('data.json', data, (err) => {
-    if (err) throw err
-    console.log('Save File')
+    fs.writeFile('data.json', data, (err) => {
+      if (err) throw err
+      console.log('Save File')
+    })
   })
-// console.log(res)
-})
+
+  /*
+    MeasureType: 'Scoring', // several other options 
+    PerMode: 'PerGame',
+    PlusMinus: '2544',
+    PaceAdjust: null, //
+    Rank: null, //
+    Season: '2015-2016', // 2016-2017
+    SeasonType: 'Regular Season',
+    Outcome: null,
+    Location: null,
+    Month: null,
+    SeasonSegment: null,
+    DateFrom: '10/4/2016', // MM/DD/YYYY
+    DateTo: '11/15/2016', //
+    OpponentTeamID: null, //
+    VsConference: null, //
+    VsDivsion: null, //
+    GameSegment: null,
+    Period: '0', // '-13'
+    LastNGames: '2', // 'n'
+    TeamId: '1610612755'
+
+  })
+  */
