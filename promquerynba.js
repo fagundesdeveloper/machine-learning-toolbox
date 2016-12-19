65cf7308cf36482c1d9aa53caa9ac8d53cd2b3c9
nicholas <nohman@gmail.com>
2016-12-18 20:58:41 -0800

    Adds counting stats

--- promquerynba.js
+++ promquerynba.js
@@ -12,7 +12,7 @@ function makepromCalls(key) {
       for (season in endjson[key.name]){
         endjson[key.name][season].season_id = parseInt(endjson[key.name][season].season_id.substring(0, 4))
         endjson[key.name][season].trueShooting = (endjson[key.name][season].pts / (2 * (endjson[key.name][season].fga + (0.44 * endjson[key.name][season].fta)))).toFixed(3)
-
+        endjson[key.name][season].countingStats = (endjson[key.name][season].reb + endjson[key.name][season].ast + endjson[key.name][season].blk + endjson[key.name][season].stl).toFixed(1)
       }
       resolve([d, key])
     })
